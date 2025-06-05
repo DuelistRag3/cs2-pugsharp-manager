9ade20a0279e8ab7a797a7110ee1bac51f5dbe51
DuelistRag3 <christian.dullin@web.de>
2025-06-05 12:08:23 +0200

    feat: Neue Controller und Migrationen hinzugefügt

--- resources/views/livewire/admin/tournaments/index.blade.php
+++ resources/views/livewire/admin/tournaments/index.blade.php
@@ -9,13 +9,13 @@
                         Nummer
                     </th>
                     <th scope="col" class="px-6 py-3">
-                        Color
+                        Status
                     </th>
                     <th scope="col" class="px-6 py-3">
-                        Category
+                        Teams / Max Teams
                     </th>
                     <th scope="col" class="px-6 py-3">
-                        Price
+                        Aktionen
                     </th>
                 </tr>
             </thead>
@@ -32,34 +32,6 @@
                     </td>
                     <td class="px-6 py-4">
                         $2999
-                    </td>
-                </tr>
-                <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200">
-                    <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                        Microsoft Surface Pro
-                    </th>
-                    <td class="px-6 py-4">
-                        White
-                    </td>
-                    <td class="px-6 py-4">
-                        Laptop PC
-                    </td>
-                    <td class="px-6 py-4">
-                        $1999
-                    </td>
-                </tr>
-                <tr class="bg-white dark:bg-gray-800">
-                    <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                        Magic Mouse 2
-                    </th>
-                    <td class="px-6 py-4">
-                        Black
-                    </td>
-                    <td class="px-6 py-4">
-                        Accessories
-                    </td>
-                    <td class="px-6 py-4">
-                        $99
                     </td>
                 </tr>
             </tbody>
