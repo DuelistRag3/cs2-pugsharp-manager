6005de678eddce5c13a64e123d27267046823734
DuelistRag3 <christian.dullin@web.de>
2025-06-07 20:04:22 +0200

    Fix navigation view path

--- app/View/Components/Navigation/Navbar.php
+++ app/View/Components/Navigation/Navbar.php
@@ -21,6 +21,6 @@ class Navbar extends Component
      */
     public function render(): View|Closure|string
     {
-        return view('components.navigation..navbar');
+        return view('components.navigation.navbar');
     }
 }
