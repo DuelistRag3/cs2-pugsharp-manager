15c58dd30dffc447c6dfcedc5480106b4938cff4
DuelistRag3 <christian.dullin@web.de>
2025-08-14 11:11:15 +0200

    feat: create table to dettermine which players of a team attend a tournament

---
+++ database/migrations/2025_08_14_104329_create_team_tournament_player_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('team_tournament_player', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('team_tournament_id')->constrained()->onDelete('cascade');
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('team_tournament_player');
+    }
+};
