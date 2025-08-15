716ac419b58c449476fc93b01d9e7e27763b8507
DuelistRag3 <christian.dullin@web.de>
2025-08-15 12:03:06 +0200

    feat: only allow cancelation if tournament is not running or finished

--- resources/views/livewire/tournaments/show.blade.php
+++ resources/views/livewire/tournaments/show.blade.php
@@ -6,7 +6,7 @@
             <button type="button" data-modal-target="register-modal" data-modal-toggle="register-modal"
                 class="focus:outline-none text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 cursor-pointer">{{
                 __('manager.register_team') }}</button>
-            @elseif($tournament->teams()->where('captain_id', auth()->id())->exists())
+            @elseif($tournament->teams()->where('captain_id', auth()->id())->exists() && $tournament->status === 'scheduled')
             <button type="button" wire:click='cancelRegistration({{ $tournament->teams()->where('captain_id', auth()->id())->first()->id }})'
                 class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800 cursor-pointer">{{
                 __('manager.cancel_registration') }}</button>
