090ddae158fefbed3b323eb9d17cc40899791fb2
DuelistRage <christian@christian-dullin.de>
2025-08-25 16:11:56 +0200

    fix: add TeamHasPlayer and TeamInTournament for successfull migration

---
+++ app/Models/TeamHasPlayer.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class TeamHasPlayer extends Model
+{
+
+    protected $fillable = [
+        'team_id',
+        'player_id',
+    ];
+
+    public function team(): BelongsTo
+    {
+        return $this->belongsTo(Team::class);
+    }
+
+    public function player(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
