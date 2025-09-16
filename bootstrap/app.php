5dee136fa64e5bc79750d9d4208e28561c08fbda
DuelistRag3 <christian.dullin@web.de>
2025-09-16 14:59:27 +0200

    feat: add middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,10 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->append(\App\Http\Middleware\FetchLocaleFromBrowser::class);
+        $middleware->append([
+            \App\Http\Middleware\FetchLocaleFromBrowser::class,
+            \App\Http\Middleware\Installed::class,
+        ]);
         $middleware->alias([
             'role' => \Spatie\Permission\Middleware\RoleMiddleware::class,
             'permission' => \Spatie\Permission\Middleware\PermissionMiddleware::class,
