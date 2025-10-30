6fac5cceb0202c0d656d9b844b4cf5b6ecb207f5
DuelistRag3 <christian.dullin@web.de>
2025-10-30 11:37:32 +0100

    Update to Beta v1.3.6: fix: fixed a bug that causes the database init to fail when using sqlite

--- config/app.php
+++ config/app.php
@@ -125,6 +125,6 @@ return [
         'store' => env('APP_MAINTENANCE_STORE', 'database'),
     ],
 
-    'version' => 'Beta v1.3.4'
+    'version' => 'Beta v1.3.6'
 
 ];
--- database/migrations/2025_10_01_131941_drop_game_winner_foreign_key_constrained.php
+++ database/migrations/2025_10_01_131941_drop_game_winner_foreign_key_constrained.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('games', function (Blueprint $table) {
-            $table->dropForeign('id');
+            $table->dropForeign(['id']);
         });
     }
 
