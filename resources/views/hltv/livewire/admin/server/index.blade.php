a1db55ba94258b906519c532bbabc38625b1e0ad
DuelistRag3 <christian.dullin@web.de>
2025-10-30 13:30:34 +0100

    fix: server add modal closes instantly duo to livewire

--- resources/views/hltv/livewire/admin/server/index.blade.php
+++ resources/views/hltv/livewire/admin/server/index.blade.php
@@ -97,7 +97,7 @@
         @endif
     </div>
 
-    <div id="create-modal" tabindex="-1" aria-hidden="true"
+    <div id="create-modal" tabindex="-1" aria-hidden="true" wire:ignore.self
         class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
         <div class="relative p-4 w-full max-w-2xl max-h-full">
             <div class="relative bg-white rounded-lg shadow-sm dark:bg-gray-700">
