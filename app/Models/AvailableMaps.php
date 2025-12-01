222570f3409fd30da42ebf173f236047ddde2998
DuelistRag3 <christian.dullin@web.de>
2025-12-01 12:42:35 +0100

    fix: fixed image loading for maps

--- app/Models/AvailableMaps.php
+++ app/Models/AvailableMaps.php
@@ -20,18 +20,30 @@ class AvailableMaps extends Model
      */
     public function getImageUrlAttribute()
     {
-        if (!$this->image_name) {
-            $asset = "https://raw.githubusercontent.com/ghostcap-gaming/cs2-map-images/refs/heads/main/cs2/{$this->map_code}.png";
-            if(!file_exists($asset))
-            {
-                $asset = Vite::asset("resources/images/maps.jpg"); 
-            }
-            return $asset;
+        $asset = Vite::asset("resources/images/maps.jpg");
+
+        if($this->image_name) {
+            $asset = Storage::temporaryUrl("maps_thumbnails/{$this->image_name}", now()->addMinutes(5), [
+                'ResponseContentDisposition' => 'inline; filename="' . $this->image_name . '"',
+            ]);
         }
 
-        // return asset("images/maps_thumbnails/{$this->image_name}");
-        return Storage::temporaryUrl("maps_thumbnails/{$this->image_name}", now()->addMinutes(5), [
-            'ResponseContentDisposition' => 'inline; filename="' . $this->image_name . '"',
-        ]);
+        if($this->map_code) {
+            $url = "https://raw.githubusercontent.com/ghostcap-gaming/cs2-map-images/refs/heads/main/cs2/{$this->map_code}.png";
+            $ch = curl_init();
+            curl_setopt($ch, CURLOPT_URL,$url);
+            // don't download content
+            curl_setopt($ch, CURLOPT_NOBODY, 1);
+            curl_setopt($ch, CURLOPT_FAILONERROR, 1);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+            $result = curl_exec($ch);
+            if($result !== FALSE)
+            {
+                $asset = $url;
+            }
+        }
+
+        return $asset;
     }
 }
